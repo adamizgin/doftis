bbe350c59a389bf6398321488d67151f6d68b6ce
adamizgin <88751196+adamizgin@users.noreply.github.com>
2021-12-18 16:46:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ $(document).ready(function() {
 			    $('#ps-right').css({
 			    	'bottom': '0'
 			    });
+			    $('#mobile-ps-img').css({
+			    	'top': '50%'
+			    });
 			    $('#ps-left').css({
 			    	'bottom': '0'
 			    });
@@ -20,6 +23,9 @@ $(document).ready(function() {
 				    	'opacity': '1'
 				    });
 				    $('#ps-left').css({
+				    	'opacity': '1'
+				    });
+				    $('#mobile-ps-img').css({
 				    	'opacity': '1'
 				    });
 				}, 200);
