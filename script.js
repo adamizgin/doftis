96c6fc633b6e1de51a55124db2ecd52bb2e3db37
adamizgin <88751196+adamizgin@users.noreply.github.com>
2021-12-28 00:48:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
 	$('#see-product').click(function() {
 		$('#outside-main').show();
+		if ($(window).width() <= 640) {
+			$('.canvas-computer').removeAttr('id');
+			$('.canvas-mobile').attr('id', 'canvas');
+		} else if ($(window).width() >= 640) {
+			$('.canvas-mobile').removeAttr('id');
+			$('.canvas-computer').attr('id', 'canvas');
+		}
 		setTimeout(function() {
+			setTimeout(function() {
+				$('#smoke-script').attr('src', 'smoke.js');
+			}, 500);
 			$([document.documentElement, document.body]).animate({
 		        scrollTop: $("#product-section").offset().top - 75+'px'
 		    }, 600);
